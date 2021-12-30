489ff8e4620e3a2d8489ef3b31a0bd215f6d4a9e
Robert Michaud <robert.cn.michaud@hotmail.com>
2021-12-29 21:59:47 -0500

    telescope and treesitter

--- lua/yue/keymaps.lua
+++ lua/yue/keymaps.lua
@@ -39,3 +39,10 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- VISUAL BLOCK --
+
+
+-- Telescope
+-- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
--- lua/yue/plugins.lua
+++ lua/yue/plugins.lua
@@ -71,6 +71,15 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig"
   use "williamboman/nvim-lsp-installer"
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  -- use "nvim-telescope/telescope-media-files.nvim"
+
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
 
 
 
