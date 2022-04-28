1ee420c8cf2c6e0c822bed0971f275f691968da4
Robert Michaud <robert.cn.michaud@hotmail.com>
2022-04-27 23:47:50 -0400

    Fix broken nvim-tree
    nvim tree likes to change its settings around to break my configs.
    Updated setup with new defaults with few tweaks.

--- lua/yue/nvim-tree.lua
+++ lua/yue/nvim-tree.lua
@@ -34,28 +34,66 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  auto_reload_on_write = true,
   disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  actions = {
-    open_file = {
-      resize_window = true
-    }
-  },
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
-  open_on_tab = false,
   hijack_cursor = false,
-  update_cwd = true,
-  update_to_buf_dir = {
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_buffer_on_setup = false,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  open_on_tab = false,
+  sort_by = "name",
+  update_cwd = false,
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    mappings = {
+      custom_only = false,
+      list = {
+        -- user mappings go here
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+    },
+  },
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {},
+  system_open = {
+    cmd = "",
+    args = {},
+  },
   diagnostics = {
     enable = true,
+    show_on_dirs = false,
     icons = {
       hint = "",
       info = "",
@@ -63,54 +101,130 @@ nvim_tree.setup {
       error = "",
     },
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
   filters = {
     dotfiles = false,
     custom = {},
+    exclude = {},
   },
   git = {
     enable = true,
-    ignore = false,
-    timeout = 500,
+    ignore = true,
+    timeout = 400,
   },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    auto_resize = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    open_file = {
+      quit_on_open = true,
+      resize_window = true,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
       },
     },
-    number = false,
-    relativenumber = false,
   },
   trash = {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 1,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+    },
   },
+
+  -- disable_netrw = true,
+  -- hijack_netrw = true,
+  -- open_on_setup = false,
+  -- actions = {
+  --   open_file = {
+  --     resize_window = true
+  --   }
+  -- },
+  -- ignore_ft_on_setup = {
+  --   "startify",
+  --   "dashboard",
+  --   "alpha",
+  -- },
+  -- open_on_tab = false,
+  -- hijack_cursor = false,
+  -- update_cwd = true,
+  -- update_to_buf_dir = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
+  -- diagnostics = {
+  --   enable = true,
+  --   icons = {
+  --     hint = "",
+  --     info = "",
+  --     warning = "",
+  --     error = "",
+  --   },
+  -- },
+  -- update_focused_file = {
+  --   enable = true,
+  --   update_cwd = true,
+  --   ignore_list = {},
+  -- },
+  -- system_open = {
+  --   cmd = nil,
+  --   args = {},
+  -- },
+  -- filters = {
+  --   dotfiles = false,
+  --   custom = {},
+  -- },
+  -- git = {
+  --   enable = true,
+  --   ignore = false,
+  --   timeout = 500,
+  -- },
+  -- view = {
+  --   width = 30,
+  --   height = 30,
+  --   hide_root_folder = false,
+  --   side = "left",
+  --   auto_resize = true,
+  --   mappings = {
+  --     custom_only = false,
+  --     list = {
+  --       { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+  --       { key = "h", cb = tree_cb "close_node" },
+  --       { key = "v", cb = tree_cb "vsplit" },
+  --     },
+  --   },
+  --   number = false,
+  --   relativenumber = false,
+  -- },
+  -- trash = {
+  --   cmd = "trash",
+  --   require_confirm = true,
+  -- },
+  -- quit_on_open = 1,
+  -- git_hl = 1,
+  -- disable_window_picker = 0,
+  -- root_folder_modifier = ":t",
+  -- show_icons = {
+  --   git = 1,
+  --   folders = 1,
+  --   files = 1,
+  --   folder_arrows = 1,
+  --   tree_width = 30,
+  -- },
 }
